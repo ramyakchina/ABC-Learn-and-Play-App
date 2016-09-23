eb166dc20c8e72b9ea2754d0cda008cb06b332cf
ramyareddy65 <ramya.reddi35@gmail.com>
2016-09-23 16:14:19 -0700

    first commit

---
+++ youtubefinal/NumberController.swift
@@ -0,0 +1,35 @@
+//
+//  NumberController.swift
+//  youtubefinal
+//
+//  Created by Ramya Katipelly on 7/22/16.
+//  Copyright © 2016 16545_RajashekarReddy Rela. All rights reserved.
+//
+
+import UIKit
+
+class NumberController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ youtubefinal/video3.swift
@@ -0,0 +1,35 @@
+//
+//  video3.swift
+//  youtubefinal
+//
+//  Created by RajashekarReddy Rela on 8/2/16.
+//  Copyright © 2016 16545_RajashekarReddy Rela. All rights reserved.
+//
+
+import UIKit
+
+class video3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
